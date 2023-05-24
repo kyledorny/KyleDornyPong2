c816f0e07f72cd7fe9edc74e544a2c39e2e3f7b6
Kyle Dorny <kyledorny@gmail.com>
2023-05-24 11:14:08 -0500

    Added guitar strum score sound

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -49,7 +49,7 @@ soundArray.push(wallSound); //add sound to list of sounds
 export var paddleSound = new soundFile("PaddleWood.mp3");
 soundArray.push(paddleSound);
 
-export var scoreSound = new soundFile("silence.mp3");
+export var scoreSound = new soundFile("GuitarStrum.wav");
 soundArray.push(scoreSound);
 
 export var ambientSound = new soundFile("AmbientPiano.mp3");
